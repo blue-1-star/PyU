f67445fe2cf57f7f706b51abd0eea9265f96d1e4
blue-1-star <san.tretiak@gmail.com>
2024-01-10 18:32:10 +0200

    wrk_pdf modified

--- Dubr/wrk_pdf.py
+++ Dubr/wrk_pdf.py
@@ -2,4 +2,6 @@ import pandas as pd
 import tabula
 from tabula import read_pdf
 file_path = "/mnt/g/Book/Biology/Дубровский/aref_Dubrovski.pdf"
+df = read_pdf(file_path, pages='all')
+print(f'Number of Tables=  {len(df)}')
 
